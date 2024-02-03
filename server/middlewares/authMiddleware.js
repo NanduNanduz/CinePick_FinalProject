d2a2c5f49a0542d8e90b9e1abb1ef4f8ed0f5ca0
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-02-04 00:57:11 +0530

    Update authMiddleware.js

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -2,11 +2,15 @@ const jwt = require("jsonwebtoken");
 
 module.exports = function(req,res,next) {
     try {
+        //std used to send the token from frontend to backend
         const token = req.headers.authorization.split(" ")[1];
+        //Tis secret key must match with the secret key ,which have used for the encryption 
         const decoded = jwt.verify(token, process.env.jwt_secret);
         req.body.userId = decoded.userId;
+        //next() -- Execute the logic in the actual API endpoint (usersRoute try block)
         next();
     } catch (error) {
+        //if any issues with the token
         res.status(401).send({success: false, message: "Invalid token"})
     }
 };
