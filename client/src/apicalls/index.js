14167b648bae08452e48c053994b706bb41d3b72
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-02-04 00:56:54 +0530

    Update index.js

--- client/src/apicalls/index.js
+++ client/src/apicalls/index.js
@@ -1,6 +1,6 @@
 import axios from  'axios';
 
-export const axiosInstance =axios.create({
+export const axiosInstance = axios.create({
     headers : {
         'Content-Type' : 'application/json',
         authorization  : `Bearer ${localStorage.getItem('token')}`
