6fd920be249fa9b99130acb5751ee8d8f61d4da9
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-02-04 23:11:58 +0530

    updated index.js with comments

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  //provide the store to parent component
   <Provider
   store={store}
   >
