856ab4b91df17db083ad8a8a65fb0145099ae6f0
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-02-09 21:29:15 +0530

    Update PageTitle.js

--- client/src/components/PageTitle.js
+++ client/src/components/PageTitle.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
-function PageTitle({title}) {
-  return (
-    <h1 className='text-xl uppercase'>
-        {title}
-    </h1>
-  )
+function PageTitle({ title }) {
+  return <h1 className="text-xl uppercase">{title}</h1>;
 }
 
-export default PageTitle
+export default PageTitle;
