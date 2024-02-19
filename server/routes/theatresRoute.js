eb703398c7f69922940dfaf4e497f6085d578ba0
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-02-19 21:53:48 +0530

    Created API endpoint for adding a new show.

--- server/routes/theatresRoute.js
+++ server/routes/theatresRoute.js
@@ -1,93 +1,110 @@
 const router = require("express").Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 const Theatre = require("../models/theatreModel");
-
+const Show = require("../models/showModel");
 
 //add theatre
 
- router.post("/add-theatre", async(req,res)=>{
-    try {
-        const newTheatre =  new Theatre(req.body);
-        await newTheatre.save();
-        res.send({
-            success: true,
-            message: "Theatre added successfully",
-        });     
-    } catch (error) {
-        res.send({
-            success: false,
-            message: error.message,
-        });
-    }
- });
+router.post("/add-theatre", async (req, res) => {
+  try {
+    const newTheatre = new Theatre(req.body);
+    await newTheatre.save();
+    res.send({
+      success: true,
+      message: "Theatre added successfully",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
+//get all theatre
 
- //get all theatre
+router.get("/get-all-theatres", async (req, res) => {
+  try {
+    const theatres = await Theatre.find().sort({ createdAt: -1 });
+    res.send({
+      success: true,
+      mmessage: "Theatres fetched Succesfully",
+      data: theatres,
+    });
+  } catch (error) {
+    req.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
- router.get("/get-all-theatres",async(req,res)=>{
-    try {
-        const theatres = await Theatre.find().sort({createdAt: -1});
-        res.send({
-            success: true,
-            mmessage: "Theatres fetched Succesfully",
-            data: theatres,
-        });    
-    } catch (error) {
-        req.send({
-            success: false,
-            message: error.message,
-        });
-    }
- });
-
- //get all theatres by owner
- router.post("/get-all-theatres-by-owner",async(req,res)=>{
-    try {
-        const theatres = await Theatre.find({owner: req.body.owner }).sort({createdAt: -1 });
-        res.send({
-            success: true,
-            message: "Theatres fetched successfully" ,
-            data: theatres,
-        });
-    } catch (error) {
-        res.send({
-            success: false,
-            message: error.message,          
-        });
-    }
- });
-
+//get all theatres by owner
+router.post("/get-all-theatres-by-owner", async (req, res) => {
+  try {
+    const theatres = await Theatre.find({ owner: req.body.owner }).sort({
+      createdAt: -1,
+    });
+    res.send({
+      success: true,
+      message: "Theatres fetched successfully",
+      data: theatres,
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
 // update theatre
- router.post("/update-theatre", async(req,res)=>{
-    try {
-        await Theatre.findByIdAndUpdate(req.body.theatreId, req.body);
-        res.send({
-            success: true,
-            message: "Theatre updated successfully",
-        });
-    } catch (error) {
-        res.send({
-            success: false,
-            message: error.message,
-        }); 
-    }
- });
+router.post("/update-theatre", async (req, res) => {
+  try {
+    await Theatre.findByIdAndUpdate(req.body.theatreId, req.body);
+    res.send({
+      success: true,
+      message: "Theatre updated successfully",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
- //delete theatre
- router.post("/delete-theatre", async(req,res)=>{
-    try {
-        await Theatre.findByIdAndDelete(req.body.theatreId);
-        res.send({ 
-                success: true,
-                message: "Theatre deleted successfully",
-        });
-    } catch (error) {
-        res.send({
-            success: false,
-            message: error.message,
-        });
-    }
- });
+//delete theatre
+router.post("/delete-theatre", async (req, res) => {
+  try {
+    await Theatre.findByIdAndDelete(req.body.theatreId);
+    res.send({
+      success: true,
+      message: "Theatre deleted successfully",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
 
- module.exports = router;
+//add new show
+router.post("/add-show", async (req, res) => {
+  try {
+    const newShow = new Show(req.body);
+    await newShow.save();
+    res.send({
+      success: true,
+      message: "Show added successfully",
+    });
+  } catch (error) {
+    res.send({
+      success: false,
+      message: error.message,
+    });
+  }
+});
+
+module.exports = router;
