2a44366af695116e180225efd4fb41dadc8dc955
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-02-24 00:03:05 +0530

    Created a folder to show the list of theaters where a particular movie is running.

--- client/src/App.js
+++ client/src/App.js
@@ -11,25 +11,56 @@ import ProtectedRoute from './components/ProtectedRoute';
 import { useSelector } from 'react-redux';
 import Profile from './pages/Profile';
 import Admin from './pages/Admin';
+import TheatresForMovie from './pages/TheatresForMovie';
 
 
 
 function App() {
   const {loading} = useSelector((state) => state.loaders);
   return (
-    <div >
+    <div>
       {loading && (
-      <div className='loader-parent'>
-        <div className='loader'></div>
+        <div className="loader-parent">
+          <div className="loader"></div>
         </div>
-        )}
+      )}
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>} />
-          <Route path="/profile" element={<ProtectedRoute><Profile/></ProtectedRoute>} />
-          <Route path="/admin" element={<ProtectedRoute><Admin/></ProtectedRoute>} />
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
+          <Route
+            path="/"
+            element={
+              <ProtectedRoute>
+                <Home />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/movie/:id"
+            element={
+              <ProtectedRoute>
+                <TheatresForMovie />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute>
+                <Admin />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ client/src/pages/TheatresForMovie/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function TheatresForMovie() {
+    const[movie, setMovie] = React.useState(null)
+  return (
+    <div>
+      TheatresForMovie
+    </div>
+  )
+}
+
+export default TheatresForMovie
