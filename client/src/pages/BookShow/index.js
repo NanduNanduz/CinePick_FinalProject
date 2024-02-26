94a52727c0d9940c0dd4d363c3ff097f99b895b0
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-02-26 22:16:47 +0530

    created seat arrangements for booking shows and added headers with the theater name, address, movie title, date, and time of the movie to be booked

--- client/src/pages/BookShow/index.js
+++ client/src/pages/BookShow/index.js
@@ -1,11 +1,91 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useParams } from "react-router-dom";
+import { HideLoading, ShowLoading } from "../../redux/loadersSlice";
+import { message } from "antd";
+import { GetShowById } from "../../apicalls/theatres";
+import moment from "moment";
 
 function BookShow() {
+  const [show, setShow] = React.useState(null);
+  const params = useParams();
+  const dispatch = useDispatch();
+
+  const getData = async () => {
+    try {
+      dispatch(ShowLoading());
+      const response = await GetShowById({ showId: params.id });
+      if (response.success) {
+        setShow(response.data);
+      } else {
+        message.error(response.message);
+      }
+      dispatch(HideLoading());
+    } catch (error) {
+      dispatch(HideLoading());
+      message.error(error.message);
+    }
+  };
+
+  const getSeats = () => {
+    const columns = 10;
+    const totalSeats = show.totalSeats;
+    const rows = Math.ceil(totalSeats / columns);
+    //iterate through the total seats(once 12 seats are filled go to the next row (eg: 0 - 199 for 120))
+    return (
+      <div className="flex gap-1 flex-col p-2 card">
+        {Array.from(Array(rows).keys()).map((seat, index) => {
+          return (
+            <div className="flex gap-1">
+              {Array.from(Array(columns).keys()).map((column, index) => {
+                return (
+                  <div className="seat">
+                    <h1 className="text-sm">{seat * columns + column + 1}</h1>
+                  </div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
   return (
-    <div>
-      BookShow
-    </div>
-  )
+    show && ( // Check if show is not null
+      <div>
+        {/* Show Information */}
+        <div className="flex justify-between card p-2 items-center">
+          <div>
+            <h1 className="text-xl">{show.theatre.name}</h1>
+            <h1 className="text-sm">{show.theatre.address}</h1>
+          </div>
+
+          <div>
+            <h1 className="text-2xl uppercase">
+              {show.movie.title} ({show.movie.language})
+            </h1>
+          </div>
+
+          <div>
+            <h1 className="text-xl">
+              {moment(show.date).format("MMM Do yyyy")} -{" "}
+              {moment(show.time, "HH:mm").format("hh:mm A")}
+            </h1>
+          </div>
+        </div>
+
+        {/* Seats */}
+
+       <div className="flex justify-center mt-2">
+         {getSeats()}
+       </div>
+      </div>
+    )
+  );
 }
 
-export default BookShow
+export default BookShow;
