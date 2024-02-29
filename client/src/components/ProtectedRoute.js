a50d6611a2310e050acf95c898b9696d79765e08
Nandana <115874551+NanduNanduz@users.noreply.github.com>
2024-02-29 21:11:28 +0530

    token remove from the local storage and navigate to login when token expires

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -23,6 +23,8 @@ function ProtectedRoute({ children }) {
       } else {
         dispatch(SetUser(null));
         message.error(response.message);
+        localStorage.removeItem('token');
+        navigate("/login");
       }
     } catch (error) {
       dispatch(HideLoading());
